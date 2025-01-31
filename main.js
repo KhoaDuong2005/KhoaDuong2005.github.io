ffef06b98e5ac2c537aa822b7eff06295252d219
Elton <khoa.dhaminh723@gmail.com>
2025-01-30 19:07:45 -0800

    Add fetching to fetch data and add them on MongoDB Atlas

--- main.js
+++ main.js
@@ -16,3 +16,38 @@ document.getElementById("closeBtn").addEventListener("click", function () {
 document.getElementById('morelink').addEventListener('click', function(event) {
   event.preventDefault();
 });
+
+
+//submit the data to mongodb
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+
+  const data = {
+    name: name,
+    email: email,
+    message: message,
+  };
+
+
+  fetch("https://my-website-9knu.onrender.com/post", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Success");
+      alert('Message successfully sent');
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error sending message");
+    });
+});
